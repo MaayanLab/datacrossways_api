3caca2b0bda86755fb9e5bb8e4bc02e48b690d8a
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-12-15 14:10:34 -0500

    fix arg location

--- createdb.py
+++ createdb.py
@@ -1,9 +1,9 @@
 from app import db, User, Collection, Role
 import sys
 
-email= sys.argv[0]
-first_name = sys.argv[1]
-last_name = sys.argv[2]
+email= sys.argv[1]
+first_name = sys.argv[2]
+last_name = sys.argv[3]
 
 db.drop_all()
 db.create_all()
@@ -17,6 +17,9 @@ root_collection = Collection(name="root", user=user_1)
 
 admin_role = Role(name="admin")
 user_1.roles.append(admin_role)
+
+uploader_role = Role(name="uploader")
+user_1.roles.append(uploader_role)
 
 db.session.add(user_1)
 db.session.commit()
