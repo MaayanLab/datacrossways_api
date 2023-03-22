73852b48966b79130b39a5874c49b48700e46e4b
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2023-03-21 20:26:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,8 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
     config={
-        'app_name': "Seans-Python-Flask-REST-Boilerplate"
+        'app_name': "Datacrossways",
+        'displayHeader': False  # Hide the SmartBear header
     }
 )
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
