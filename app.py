9760620a9ae650cf6e9ccca1ee12ecc3525a3eac
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2023-03-27 12:53:54 -0400

    add conf redirect for oauth

--- app.py
+++ app.py
@@ -870,8 +870,7 @@ def login():
     redirect_endpoint = request.args.get('redirect_endpoint', None)
     google = oauth.create_client('google')  # create the google oauth client
     #redirect_uri = url_for('authorize', provider="google", _external=True)
-    #print(redirect_uri)
-    redirect_uri = "https://lymecommons.org/api/user/authorize?provider=google"
+    redirect_uri = conf["redirect"]+"/api/user/authorize?provider=google"
     if redirect_endpoint != None:
         session['redirect_endpoint'] = redirect_endpoint
     return google.authorize_redirect(redirect_uri)
@@ -881,7 +880,7 @@ def login_orcid():
     redirect_endpoint = request.args.get('redirect_endpoint', None)
     orcid = oauth.create_client('orcid')  # create the orcid oauth client
     #redirect_uri = url_for('authorize', provider="orcid", _external=True)
-    redirect_uri = "https://lymecommons.org/api/user/authorize?provider=orcid"
+    redirect_uri = conf["redirect"]+"/api/user/authorize?provider=orcid"
     if redirect_endpoint != None:
         session['redirect_endpoint'] = redirect_endpoint
     return orcid.authorize_redirect(redirect_uri)
